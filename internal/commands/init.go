1200312cdef2154877c78b1f3afe675545b01c53
vomit <brilgrif4004@gmail.com>
2025-10-27 23:41:55 +0000

    added init command

--- internal/commands/init.go
+++ internal/commands/init.go
@@ -1,17 +1,33 @@
 package commands
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"scripter/internal/config"
 	"scripter/internal/mainconfig"
 )
 
 func CommandInit(mainConfig *mainconfig.MainConfig) error {
-	file, err := os.Create("./scripts.json")
-	if err != nil {
-		return err
-	}
-	
+	name := "scripts.json"
 
+	if _, err := os.Stat(name); os.IsNotExist(err) {
+		defaultConfig := config.NewDefault()
+
+		data, err := json.MarshalIndent(defaultConfig, "", " ")
+		if err != nil {
+			return nil
+		}
+
+		err = os.WriteFile("scripts.json", data, 0644)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Done")
+	} else {
+		fmt.Println("Scripts file already exist!")
+	}
 	
 	return nil
 }
